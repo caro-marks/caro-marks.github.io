42c15619d2ed10e72d108dab8f1f09984f33b026
Marcos Nolasco <mksalex08@gmail.com>
2021-10-01 14:32:13 -0300

    editing data source

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
   baseURL:
-    'https://raw.githubusercontent.com/caro-marks/caro-marks.github.io/completing-portfolio/src/static/data'
+    'https://raw.githubusercontent.com/caro-marks/caro-marks.github.io/main/src/static/data'
 })

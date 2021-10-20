bf7ec5a643f2071acc3ef3e78fa31fec51149d00
Marcos Nolasco <marcosabn@gmail.com>
2021-10-20 19:04:57 -0300

    molecula de navegacao

---
+++ src/components/atoms/NavItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <a class="nav-item" :href="to">
+    {{ text }}
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    text: { type: String, required: true },
+    to: { type: String, required: true }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.nav-item {
+  color: #055612;
+  text-decoration: none;
+  font-size: 1rem;
+  padding: 0.2rem;
+  &:hover {
+    transform: scale(1.1);
+    border-radius: 10px;
+    background-color: #29c936;
+  }
+}
+</style>
---
+++ src/components/molecules/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="nav-bar">
+    <NavItem text="Skills" to="/skills" />
+    <NavItem text="Projects" to="/projects" />
+    <NavItem text="Experiences" to="/experiences" />
+    <NavItem text="About me" to="/road" />
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { NavItem } from '@/components/atoms'
+
+export default defineComponent({
+  components: {
+    NavItem
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.nav-bar {
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+}
+@media only screen and (min-width: 800px) {
+  .nav-bar {
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+  }
+}
+</style>

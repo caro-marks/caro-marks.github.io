15dfe75341dc952874cd7b5e23c50a65705ee436
Marcos Nolasco <marcosabn@gmail.com>
2021-10-20 19:06:22 -0300

    molecula de networks

---
+++ src/components/molecules/SocialNetwork.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="social-networks">
+    <SocialLink
+      name="Github"
+      icon="github.svg"
+      to="https://www.github.com/caro-marks"
+    />
+    <SocialLink
+      name="Linkedin"
+      icon="linkedin.svg"
+      to="https://www.linkedin.com/in/caro-marks"
+    />
+    <SocialLink
+      name="Twitter"
+      icon="twitter.svg"
+      to="https://www.twitter.com/carom4rks"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { SocialLink } from '@/components/atoms'
+
+export default defineComponent({
+  components: {
+    SocialLink
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.social-networks {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: 1rem;
+  justify-items: center;
+}
+</style>

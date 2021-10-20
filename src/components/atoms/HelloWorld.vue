1731beb3dab6387674350078180889a7750ed7a0
Marcos Nolasco <marcosabn@gmail.com>
2021-10-20 19:09:31 -0300

    header organism

---
+++ src/components/atoms/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="typewriter">
+    <h1>
+      {{ text }}
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      text: 'Hello World'
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.typewriter {
+  width: max-content;
+  h1 {
+    overflow: hidden; /* Ensures the content is not revealed until the animation */
+    color: #055612;
+    font-size: 3rem;
+    border-right: 0.15em solid #91c321; /* The typwriter cursor */
+    white-space: nowrap; /* Keeps the content on a single line */
+    margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+    letter-spacing: 0.15em; /* Adjust as needed */
+    animation: typing 3.5s steps(40, end), blink-caret 0.75s step-end infinite;
+  }
+}
+@keyframes typing {
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
+}
+@keyframes blink-caret {
+  from,
+  to {
+    border-color: transparent;
+  }
+  50% {
+    border-color: #91c321;
+  }
+}
+</style>
---
+++ src/components/organisms/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="header">
+    <div class="logo">
+      <HelloWorld />
+    </div>
+    <div class="bar">
+      <NavBar />
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { HelloWorld } from '@/components/atoms'
+import { NavBar } from '@/components/molecules'
+
+export default defineComponent({
+  components: {
+    HelloWorld,
+    NavBar
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  align-items: center;
+  // justify-content: space-around;
+  .logo {
+    grid-row: 2;
+  }
+}
+
+@media only screen and (min-width: 700px) {
+  .header {
+    display: grid;
+    grid-template-columns: 340px auto;
+    .logo {
+      grid-row: 1;
+    }
+  }
+}
+</style>

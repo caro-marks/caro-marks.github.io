3e65603b0c378c344759e373dd695968473ef1b4
Marcos Nolasco <marcosabn@gmail.com>
2021-11-17 17:11:12 -0300

    new homepage interface

---
+++ components/molecules/SkillView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="skillsview">
+    <div class="skillview">
+      <div class="top">
+        <img src="@/assets/images/icons/front.png" alt="front icon" />
+        <h3>Front-End</h3>
+      </div>
+      <p>
+        I'm able to build designed interfaces, with responsivity and a bit of
+        UX.
+      </p>
+    </div>
+    <div class="skillview">
+      <div class="top">
+        <img src="@/assets/images/icons/back.png" alt="back icon" />
+        <h3>Back-End</h3>
+      </div>
+      <p>
+        I'm experienced with API build and integration, database modelling,
+        refactoring, and other cool stuffs.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.skillsview {
+  display: grid;
+  grid-template-columns: 25em;
+  row-gap: 1rem;
+  .skillview {
+    display: grid;
+    justify-items: center;
+    text-align: center;
+    border: 1px solid color('dark');
+    border-radius: 3rem;
+    .top {
+      display: grid;
+      grid-auto-flow: column;
+      align-items: center;
+      justify-content: space-between;
+      column-gap: 2rem;
+      img {
+        max-width: 4rem;
+      }
+    }
+  }
+}
+</style>
--- components/organisms/Presentation.vue
+++ components/organisms/Presentation.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="presentation">
-    <div class="hey">
+    <div class="left-top">
       <img class="selfie" src="@/assets/images/selfie.jpeg" alt="Me smiling!" />
-    </div>
-    <div class="ho">
-      <div class="my-name">
-        <span>My name is </span>
-        <div class="logo">MARCOS.</div>
+      <div class="introducin">
+        <h1 class="name">Hi, I'm Marcos.</h1>
         <ul class="roles">
           <li class="role">Developer</li>
           <li class="role">Physicist</li>
@@ -14,64 +11,85 @@
         </ul>
       </div>
     </div>
+    <div class="right-bot">
+      <SkillView />
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .presentation {
-  height: max-content;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  align-items: center;
-  margin: 0.95rem auto -1em;
-  .hey {
-    justify-self: center;
-  }
-  .selfie {
-    max-width: 13rem;
-    border-radius: 150px;
-  }
-  .ho {
+  margin: 0.95rem auto;
+  gap: 5em;
+  .left-top {
     display: grid;
-    grid-template-rows: 1fr 40%;
-    text-align: center;
-    align-items: center;
-    .my-name {
-      display: grid;
-      grid-template-rows: 15% 35% auto;
-      row-gap: 1rem;
-      color: color('dark', 'darkest');
-      span {
-        font-size: 2rem;
-      }
-      .logo {
-        font-size: 3rem;
-        font-weight: bold;
-      }
-      ul {
-        display: grid;
-        grid-auto-rows: 2em;
-        row-gap: 0.75em;
-        li {
-          font-size: 1.5rem;
-        }
-      }
-    }
-  }
-}
-@include screen('small', 'medium') {
-  .presentation {
-    grid-template-rows: 1fr 1fr;
-    grid-template-columns: 1fr;
     justify-items: center;
-    .ho {
-      .my-name {
-        ul {
-          grid-auto-flow: column;
-          column-gap: 0.75em;
-        }
+    gap: 1rem;
+    .selfie {
+      max-width: 13rem;
+      border-radius: 100%;
+    }
+    .introducin {
+      display: grid;
+      row-gap: 1rem;
+      justify-items: center;
+      .roles {
+        display: grid;
+        row-gap: 0.5rem;
+        font-size: 1.8rem;
       }
     }
   }
+  .right-bot {
+    display: grid;
+    justify-content: center;
+  }
 }
+// .presentation {
+//   height: max-content;
+//   .ho {
+//     display: grid;
+//     grid-template-rows: 1fr 40%;
+//     text-align: center;
+//     align-items: center;
+//     .my-name {
+//       display: grid;
+//       grid-template-rows: 15% 35% auto;
+//       row-gap: 1rem;
+//       color: color('dark', 'darkest');
+//       span {
+//         font-size: 2rem;
+//       }
+//       .logo {
+//         font-size: 3rem;
+//         font-weight: bold;
+//       }
+//       ul {
+//         display: grid;
+//         grid-auto-rows: 2em;
+//         row-gap: 0.75em;
+//         li {
+//           font-size: 1.5rem;
+//         }
+//       }
+//     }
+//   }
+// }
+// @include screen('small', 'medium') {
+//   .presentation {
+//     grid-template-rows: 1fr 1fr;
+//     grid-template-columns: 1fr;
+//     justify-items: center;
+//     .ho {
+//       .my-name {
+//         ul {
+//           grid-auto-flow: column;
+//           column-gap: 0.75em;
+//         }
+//       }
+//     }
+//   }
+// }
 </style>

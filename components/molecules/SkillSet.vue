82b00164f8fd4de8dc746d1653ee4e839e0c305d
Marcos Nolasco <marcosabn@gmail.com>
2022-08-04 16:38:13 -0300

    finishing about me page layout but content

---
+++ components/molecules/SkillSet.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="d-flex flex-column mt-3 py-2">
+    <div class="d-flex justify-content-center">
+      <b-button v-b-toggle.collapse-skills class="more text-center p-1 w-50">
+      Skills
+    </b-button>
+    </div>
+    <b-collapse id="collapse-skills" class="mt-2 skillset">
+      <Skill name="Physics" rate="92%" class-name="phys" />
+      <Skill name="English" rate="90%" class-name="eng" />
+      <Skill name="Python" rate="86%" class-name="py" />
+      <Skill name="Django" rate="80%" class-name="dj" />
+      <Skill name="Javascript" rate="77%" class-name="js" />
+      <Skill name="Html" rate="75%" class-name="html" />
+      <Skill name="Css" rate="73%" class-name="css" />
+      <Skill name="Vue" rate="72%" class-name="vue" />
+      <Skill name="Git" rate="69%" class-name="git" />
+      <Skill name="Typescript" rate="58%" class-name="ts" />
+      <Skill name="Docker" rate="46%" class-name="dk" />
+      <Skill name="AWS" rate="42%" class-name="aws" />
+      <Skill name="Design" rate="27%" class-name="des" />
+    </b-collapse>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.skillset {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(333px, 1fr));
+  grid-auto-rows: auto;
+  column-gap: 1em;
+  background-color: rgba(color('light', 'darkest'), 0.5);
+  border-radius: 3rem;
+}
+.more {
+  background-color: rgba(color('light', 'darkest'), 0.8);
+  border: none;
+  border-radius: 3rem;
+  color: rgb(225, 200, 225);
+  font-weight: bold;
+  box-shadow: 1px 1px 10px 1px rgba(color('light', 'lightest'), 0.65);
+  &:hover {
+    box-shadow: 1px 1px 15px 2px rgba(color('light', 'lightest'), 0.65);
+  }
+}
+</style>
--- components/organisms/Resume.vue
+++ components/organisms/Resume.vue
@@ -1,74 +1,16 @@
 <template>
-  <div class="resume my-4">
+  <div class="my-4">
     <h3>Hi, there! Please to meet me (;</h3>
 
     <SkillView />
+
+    <SkillSet />
     
-    <br />
-    <div class="d-flex justify-content-center">
-      <b-button v-b-toggle.collapse-data class="more w-50 text-center p-1">
-        Ideas and Ideals
-      </b-button>
-    </div>
-    <b-collapse id="collapse-data" class="mt-4">
-      <div role="tablist">
-        <div class="d-flex justify-content-center p-1" role="tab"><b-button v-b-toggle.whoami class="more w-75"><h5>Who and where am I?</h5></b-button></div>
-        <b-collapse id="whoami">
-          <p class="text-center">I'm Marcos Nolasco, 28-y brazilian, born at Porto Velho - RO, and living
-          nowadays at Florianópolis - SC.</p>
-        </b-collapse>
-
-        <div class="d-flex justify-content-center p-1" role="tab"><b-button v-b-toggle.hobbies class="more w-75"><h5>What hobbies do I have?</h5></b-button></div>
-        <b-collapse id="hobbies">
-          <p class="text-center">I've been a compulsive reader since childhood, and I enjoy to watch movies
-          a lot.</p>
-        </b-collapse>
-
-        <div class="d-flex justify-content-center p-1" role="tab"><b-button v-b-toggle.wanties class="more w-75"><h5>What do I want most?</h5></b-button></div>
-        <b-collapse id="wanties">
-          <p class="text-center">I hope a better world for those who will come, so I wanna help with
-          educational status improvement. I also wanna travel all Earth around.</p>
-        </b-collapse>
-
-        <div class="d-flex justify-content-center p-1" role="tab"><b-button v-b-toggle.donies class="more w-75"><h5>What have I done?</h5></b-button></div>
-        <b-collapse id="donies">
-          <p class="text-center">I graduated at Physics college some months ago. I used to do research
-          about complex biophysics systems using Python, and teach classes for Enem
-          exams as volunteer.</p>
-        </b-collapse>
-        
-        <div class="d-flex justify-content-center p-1" role="tab"><b-button v-b-toggle.doinges class="more w-75"><h5>What have I been doing?</h5></b-button></div>
-        <b-collapse id="doinges">
-          <p class="text-center">Since half of 2020 I've been working to become a full-stack developer. I
-          did some jobs as backend using Django, and also some personal projects as
-          frontend using Vue.</p>
-        </b-collapse>
-        
-        <div class="d-flex justify-content-center p-1" role="tab"><b-button v-b-toggle.goinges class="more w-75"><h5>Where am I up to?</h5></b-button></div>
-        <b-collapse id="goinges">
-          <p class="text-center">I'm thrilling to be healthier, happier, and to achieve financial freedom.
-          Have I said that I wanna travel the world?</p>
-        </b-collapse>
-      </div>
-    </b-collapse>
+    <Quests />
   </div>
 </template>
 <style lang="scss" scoped>
-p {
-  color: rgba(color('light'), 0.9);
-}
 h3 {
   color: color('light')
-}
-h4 {
-  color: rgba(rgb(225, 200, 225), 0.75);
-  text-shadow: 0px 0px 1px rgba(color('light'), .5),
-    0px 0px 2px rgba(color('light','lightest'), 0.25)
-}
-.more {
-  background-color: rgba(color('light', 'darkest'), 0.8);
-  border-radius: 3rem;
-  color: rgb(225, 200, 225);
-  font-weight: bold;
 }
 </style>

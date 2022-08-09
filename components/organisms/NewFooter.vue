7bfd78d3995235a33199eb377467032c7a7109e7
Ronaldo Fenomeno <marcosabn@gmail.com>
2022-08-09 19:35:02 -0300

    new footer

--- components/organisms/NewFooter.vue
+++ components/organisms/NewFooter.vue
@@ -1,8 +1,21 @@
 <template>  
-  <b-row class="new-footer py-2 px-1">
-    FOOTER
+  <b-row class="new-footer py-2 px-1 d-flex justify-content-around">
+    <div v-for="item in footList" :key="item.id">
+      {{ item }}
+    </div>
+    
   </b-row>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data: () => {
+    return {
+      footList: ['Contact me!', 'E-mail: marcosabn08@gmail.com', 'WhatsApp: +55 48 93618 0541']
+    }
+  }
+})
+</script>
 <style lang="scss">
   .new-footer {
     background-color: color('dark', 'darkest');

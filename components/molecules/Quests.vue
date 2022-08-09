053b6982621e8b89f1d6764ffcca87feba6cea66
Ronaldo Fenomeno <marcosabn@gmail.com>
2022-08-09 16:58:40 -0300

    quests

--- components/molecules/Quests.vue
+++ components/molecules/Quests.vue
@@ -9,34 +9,34 @@
       <div class="accordion" role="tablist">
         <div class="d-flex justify-content-center p-1" role="tab"><b-button v-b-toggle.whoami class="more w-75"><h5>Who and where am I?</h5></b-button></div>
         <b-collapse id="whoami" visible accordion="q-accordion">
-          <p class="text-center">I'm Marcos Nolasco, 28-y brazilian, born at Porto Velho - RO, and living
+          <p class="text-center">I'm Marcos Nolasco, 29-y brazilian, born at Porto Velho - RO, and living
           nowadays at Florianópolis - SC.</p>
         </b-collapse>
 
         <div class="d-flex justify-content-center p-1" role="tab"><b-button v-b-toggle.hobbies class="more w-75"><h5>What hobbies do I have?</h5></b-button></div>
         <b-collapse id="hobbies" accordion="q-accordion">
-          <p class="text-center">I've been a compulsive reader since childhood, and I enjoy to watch movies
+          <p class="text-center">I've been a passionate reader since childhood, and I enjoy to watch movies and series
           a lot.</p>
         </b-collapse>
 
         <div class="d-flex justify-content-center p-1" role="tab"><b-button v-b-toggle.wanties class="more w-75"><h5>What do I want most?</h5></b-button></div>
         <b-collapse id="wanties" accordion="q-accordion">
           <p class="text-center">I hope a better world for those who will come, so I wanna help with
-          educational status improvement. I also wanna travel all Earth around.</p>
+          educational status improvement.</p>
         </b-collapse>
 
         <div class="d-flex justify-content-center p-1" role="tab"><b-button v-b-toggle.donies class="more w-75"><h5>What have I done?</h5></b-button></div>
         <b-collapse id="donies" accordion="q-accordion">
-          <p class="text-center">I graduated at Physics college some months ago. I used to do research
+          <p class="text-center">I graduated at Physics college in early 2021. I used to do research
           about complex biophysics systems using Python, and teach classes for Enem
-          exams as volunteer.</p>
+          exams as volunteer. </p>
         </b-collapse>
         
         <div class="d-flex justify-content-center p-1" role="tab"><b-button v-b-toggle.doinges class="more w-75"><h5>What have I been doing?</h5></b-button></div>
         <b-collapse id="doinges" accordion="q-accordion">
-          <p class="text-center">Since half of 2020 I've been working to become a full-stack developer. I
-          did some jobs as backend using Django, and also some personal projects as
-          frontend using Vue.</p>
+          <p class="text-center"> I
+          did some jobs as backend using Django and Sanic, and some jobs as frontend
+          frontend using React and Vue.</p>
         </b-collapse>
         
         <div class="d-flex justify-content-center p-1" role="tab"><b-button v-b-toggle.goinges class="more w-75"><h5>Where am I up to?</h5></b-button></div>
